570744514a52bf4a2cdadffe7187c6a5d6d840f3
小猪之最 <59649644+TmallKing1@users.noreply.github.com>
2023-05-30 18:37:11 +0800

    Disable The End

---
+++ src/main/java/top/pigest/disabletheend/DisableTheEnd.java
@@ -0,0 +1,11 @@
+package top.pigest.disabletheend;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class DisableTheEnd implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
---
+++ src/main/java/top/pigest/disabletheend/mixin/EndPortalBlockMixin.java
@@ -0,0 +1,24 @@
+package top.pigest.disabletheend.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.EndPortalBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EndPortalBlock.class)
+public class EndPortalBlockMixin {
+
+    /**
+     * @author xiaozhu_zhizui
+     * @reason Disable The End
+     */
+    @Inject(method = "onEntityCollision",at = @At("HEAD"), cancellable = true)
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
