da34a570cf7254ea57df323ca8539a289aca2ec4
小猪之最 <59649644+TmallKing1@users.noreply.github.com>
2023-08-12 11:10:54 +0800

    修复后台无法使用say等命令的问题

--- src/main/java/top/pigest/disabletheend/mixin/PlayerManagerMixin.java
+++ src/main/java/top/pigest/disabletheend/mixin/PlayerManagerMixin.java
@@ -4,14 +4,11 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
-import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -38,7 +35,7 @@ public abstract class PlayerManagerMixin {
     @Inject(method = "broadcast(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/network/message/MessageType$Parameters;)V", at= @At(value = "HEAD"), cancellable = true)
     private void broadcast(SignedMessage message, ServerCommandSource source, MessageType.Parameters params, CallbackInfo ci) throws CommandSyntaxException {
         Registry<MessageType> registry = source.getRegistryManager().get(RegistryKeys.MESSAGE_TYPE);
-        if(Mute.isMutedWithExpiry(source.getPlayerOrThrow().getGameProfile()) && (registry.get(MessageType.SAY_COMMAND) == params.type() || registry.get(MessageType.EMOTE_COMMAND) == params.type())) {
+        if(source.isExecutedByPlayer() && Mute.isMutedWithExpiry(source.getPlayerOrThrow().getGameProfile()) && (registry.get(MessageType.SAY_COMMAND) == params.type() || registry.get(MessageType.EMOTE_COMMAND) == params.type())) {
             Mute.sendMuteMessage(source.getPlayerOrThrow());
             ci.cancel();
         }
